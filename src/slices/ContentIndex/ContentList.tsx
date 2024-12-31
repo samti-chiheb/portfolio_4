dc0940018db37fbcfbf3dcae9cfe9ab2dcb28f89
chihebgit <chiheb.samti@gmail.com>
2024-12-31 13:29:59 +0100

    Fix ref type

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -142,7 +142,7 @@ export default function ContentList({
         {items.map((post, index) => (
           <li
             key={index}
-            ref={(el) => (itemsRef.current[index] = el)}
+            ref={(el) =>{ itemsRef.current[index] = el}}
             onMouseEnter={() => onMouseEnter(index)}
             className="list-item opacity-0"
           >
